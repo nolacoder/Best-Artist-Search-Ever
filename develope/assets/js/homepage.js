60c3581ce702af9b430b7fada5aeec46d75b1b38
Roy <loganroyjr4@gmail.com>
2022-09-13 11:39:18 -0500

    revised HTML and JS

--- develope/assets/js/homepage.js
+++ develope/assets/js/homepage.js
@@ -1,36 +1,36 @@
-var userFormEl = document.querySelector('#user-form');
-var languageButtonsEl = document.querySelector('#language-buttons');
-var nameInputEl = document.querySelector('#username');
-var repoContainerEl = document.querySelector('#repos-container');
-var repoSearchTerm = document.querySelector('#repo-search-term');
+var searchFormEl = $('#search-form');
+var centuryButtonsEl = $('#century-buttons');
+var artistInputEl = $('#artist');
+var artworkContainerEl = $('#artwork-container');
+var artistSearchTerm = $('#artist-search-term');
 
 var formSubmitHandler = function (event) {
   event.preventDefault();
 
-  var username = nameInputEl.value.trim();
+  var artist = $("#artist").val().trim();
 
-  if (username) {
-    getUserRepos(username);
+  if (artist) {
+    getArtist(artist);
 
-    repoContainerEl.textContent = '';
-    nameInputEl.value = '';
+    artworkContainerEl.textContent = '';
+    artistInputEl.value = '';
   } else {
-    alert('Please enter a GitHub username');
+    alert('Please enter an artist name');
   }
 };
 
 var buttonClickHandler = function (event) {
-  var language = event.target.getAttribute('data-language');
+  var century = event.target.getAttribute('data-century');
 
-  if (language) {
-    getFeaturedRepos(language);
+  if (century) {
+    getCentury(century);
 
-    repoContainerEl.textContent = '';
+    artworkContainerEl.textContent = '';
   }
 };
 
-var getUserRepos = function (user) {
-  var apiUrl = 'https://api.github.com/users/' + user + '/repos';
+var getArtist = function (artist) {
+  var apiUrl = "https://openaccess-api.clevelandart.org/api/artworks?limit=20&artist="+ artist;
 
   fetch(apiUrl)
     .then(function (response) {
@@ -38,66 +38,16 @@ var getUserRepos = function (user) {
         console.log(response);
         response.json().then(function (data) {
           console.log(data);
-          displayRepos(data, user);
+          // display
         });
       } else {
         alert('Error: ' + response.statusText);
       }
     })
     .catch(function (error) {
-      alert('Unable to connect to GitHub');
+      alert('Unable to connect to Cleveland API');
     });
 };
 
-var getFeaturedRepos = function (language) {
-  var apiUrl = 'https://api.github.com/search/repositories?q=' + language + '+is:featured&sort=help-wanted-issues';
-
-  fetch(apiUrl).then(function (response) {
-    if (response.ok) {
-      response.json().then(function (data) {
-        displayRepos(data.items, language);
-      });
-    } else {
-      alert('Error: ' + response.statusText);
-    }
-  });
-};
-
-var displayRepos = function (repos, searchTerm) {
-  if (repos.length === 0) {
-    repoContainerEl.textContent = 'No repositories found.';
-    return;
-  }
-
-  repoSearchTerm.textContent = searchTerm;
-
-  for (var i = 0; i < repos.length; i++) {
-    var repoName = repos[i].owner.login + '/' + repos[i].name;
-
-    var repoEl = document.createElement('a');
-    repoEl.classList = 'list-item flex-row justify-space-between align-center';
-    repoEl.setAttribute('href', './single-repo.html?repo=' + repoName);
-
-    var titleEl = document.createElement('span');
-    titleEl.textContent = repoName;
-
-    repoEl.appendChild(titleEl);
-
-    var statusEl = document.createElement('span');
-    statusEl.classList = 'flex-row align-center';
-
-    if (repos[i].open_issues_count > 0) {
-      statusEl.innerHTML =
-        "<i class='fas fa-times status-icon icon-danger'></i>" + repos[i].open_issues_count + ' issue(s)';
-    } else {
-      statusEl.innerHTML = "<i class='fas fa-check-square status-icon icon-success'></i>";
-    }
-
-    repoEl.appendChild(statusEl);
-
-    repoContainerEl.appendChild(repoEl);
-  }
-};
-
-userFormEl.addEventListener('submit', formSubmitHandler);
-languageButtonsEl.addEventListener('click', buttonClickHandler);
+searchFormEl.on('submit', formSubmitHandler);
+centuryButtonsEl.on('click', buttonClickHandler);
